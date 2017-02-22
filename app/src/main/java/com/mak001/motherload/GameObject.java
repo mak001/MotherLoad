319bb483e16ba884bfc0c3defcbec1053ad52ec0
mak001 <mak001001@gmail.com>
2017-02-22 11:52:51 -0600

    Initial commit

---
+++ app/src/main/java/com/mak001/motherload/GameObject.java
@@ -0,0 +1,13 @@
+package com.mak001.motherload;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Matthew on 2/21/2017.
+ */
+public interface GameObject {
+
+    public void draw(Canvas canvas);
+
+    public void update();
+}
