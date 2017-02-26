d4104a5796c066775db68e2ed8c6672aec2e4aed
mak001 <mak001001@gmail.com>
2017-02-26 08:38:09 -0600

    TileTypes can now have multiple images    - world now stores tiles as floats       - tile id is stored before decimal, image index stored after

--- app/src/main/java/com/mak001/motherload/game/world/TileType.java
+++ app/src/main/java/com/mak001/motherload/game/world/TileType.java
@@ -6,6 +6,8 @@ import android.graphics.BitmapFactory;
 import com.mak001.motherload.R;
 import com.mak001.motherload.game.Constants;
 
+import java.util.Random;
+
 /**
  * Created by Matthew on 2/23/2017.
  */
@@ -19,7 +21,9 @@ public enum TileType {
     private final int minDepth, maxDepth;
     private final int hardness;
     private final int damage;
-    private final Bitmap image;
+
+    private final Bitmap[] images;
+
 
     TileType() {
         this(-1);
@@ -38,28 +42,35 @@ public enum TileType {
     }
 
     TileType(int minDepth, int maxDepth, int hardness, int damage, int imageID) {
+        this(minDepth, maxDepth, hardness, damage, new int[]{imageID});
+    }
+
+    TileType(int minDepth, int maxDepth, int hardness, int damage, int[] imageIDs) {
+
         this.id = this.ordinal();
         this.minDepth = minDepth;
         this.hardness = hardness;
         this.maxDepth = maxDepth;
         this.damage = damage;
 
-        if (imageID == -1) {
-            this.image = null;
-        } else {
-            Bitmap image = BitmapFactory.decodeResource(Constants.RESOURCES, imageID);
-            this.image = Bitmap.createScaledBitmap(image, 96, 96, false);
-            //this.image = image;
+        images = new Bitmap[imageIDs.length];
+
+        for (int i = 0; i < imageIDs.length; i++) {
+            if (imageIDs[i] == -1) {
+                images[i] = null;
+            } else {
+                Bitmap image = BitmapFactory.decodeResource(Constants.RESOURCES, imageIDs[i]);
+                images[i] = Bitmap.createScaledBitmap(image, 96, 96, false);
+            }
         }
-        //R.drawable;
     }
 
     public static TileType getIndex(int i) {
         return TileType.values()[i];
     }
 
-    public int getID() {
-        return id;
+    public float getID() {
+        return id + ((int) (Math.random() * images.length) / 100f);
     }
 
     public int getMinDepth() {
@@ -79,7 +90,11 @@ public enum TileType {
     }
 
     public Bitmap getImage() {
-        return image;
+        return images[0];
+    }
+
+    public Bitmap getImage(int index) {
+        return images[index];
     }
 
 }
--- app/src/main/java/com/mak001/motherload/game/world/World.java
+++ app/src/main/java/com/mak001/motherload/game/world/World.java
@@ -12,7 +12,7 @@ import com.mak001.motherload.game.Renderable;
 public class World implements Renderable {
 
     private final int tileWidth, tileHeight;
-    private int[][] tiles;
+    private float[][] tiles;
     private int lastGeneratedLayer = 0;
 
 
@@ -20,26 +20,20 @@ public class World implements Renderable {
         this.tileWidth = TileType.DIRT.getImage().getWidth();
         this.tileHeight = TileType.DIRT.getImage().getHeight();
 
-        tiles = new int[25][500];
+        tiles = new float[25][500];
 
         generate(tiles.length);
     }
 
-    private boolean drew = false;
-
     @Override
     public void draw(Canvas canvas) {
         // TODO
-        if (!drew) {
             Paint paint = new Paint();
-            for (int x = 0; x < tiles.length; x++) {
-                for (int y = 0; y < lastGeneratedLayer; y++) {
-                    TileType tile = TileType.getIndex(tiles[x][y]);
-                    if (tile.getImage() != null) {
-                        canvas.drawBitmap(tile.getImage(), x * tileWidth, y * tileHeight, paint);
-                    } else {
-                        System.out.println("image null at: " + x + ", " + y);
-                    }
+        for (int x = 0; x < tiles.length; x++) {
+            for (int y = 0; y < lastGeneratedLayer; y++) {
+                TileType tile = TileType.getIndex((int) tiles[x][y]);
+                if (tile.getImage() != null) {
+                    canvas.drawBitmap(tile.getImage(), x * tileWidth, y * tileHeight, paint);
                 }
             }
         }
@@ -47,7 +41,7 @@ public class World implements Renderable {
 
     public int getTileAt(int x, int y) {
         if (x >= 0 && y >= 0) {
-            return tiles[x][y];
+            return (int) tiles[x][y];
         } else {
             // 0 is air
             return 0;
