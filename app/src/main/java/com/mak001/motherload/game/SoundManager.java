9fed6b44ec8e1f5696ea793d8c40c11dbbb6c9af
mak001 <mak001001@gmail.com>
2017-03-07 13:48:53 -0600

    Updated gradle New min android version Updated SoundManager to handle new and old versions

--- app/src/main/java/com/mak001/motherload/game/SoundManager.java
+++ app/src/main/java/com/mak001/motherload/game/SoundManager.java
@@ -1,10 +1,9 @@
 package com.mak001.motherload.game;
 
-import android.content.Context;
 import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.SoundPool;
-
-import com.mak001.motherload.R;
+import android.os.Build;
 
 /**
  * Created by Matthew on 2/27/2017.
@@ -14,14 +13,18 @@ public class SoundManager {
 
     private static SoundPool soundPool;
 
+    @SuppressWarnings("deprecation")
     public SoundManager() {
+        if (Build.VERSION.SDK_INT > 20) {
+            AudioAttributes atts = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED).build();
+            soundPool = new SoundPool.Builder().setMaxStreams(5).setAudioAttributes(atts).build();
 
-        AudioAttributes atts = new AudioAttributes.Builder()
-                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                .setUsage(AudioAttributes.USAGE_GAME)
-                .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED).build();
-        soundPool = new SoundPool.Builder().setMaxStreams(5).setAudioAttributes(atts).build();
-
+        } else {
+            soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        }
     }
 
     public void setOnLoadCompleteListener(SoundPool.OnLoadCompleteListener onLoadCompleteListener) {
